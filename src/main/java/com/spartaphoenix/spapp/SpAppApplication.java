dd7862ba67782e2d32804d4f2243b358b8d6d3b1
Jordan Olney <jolney@deloitte.co.uk>
2020-08-16 13:41:51 +0100

    Initial commit

---
+++ src/main/java/com/spartaphoenix/spapp/SpAppApplication.java
@@ -0,0 +1,13 @@
+package com.spartaphoenix.spapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartaphoenix/spapp/SpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaphoenix.spapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
