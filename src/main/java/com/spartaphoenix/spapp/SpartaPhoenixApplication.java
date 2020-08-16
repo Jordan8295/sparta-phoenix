3d968e8178c888b93565906f2fc870112e249355
Jordan8295 <jordan_olney [at] hotmail.co.uk>
2020-08-16 17:27:58 +0100

    Placeholder website

--- src/main/java/com/spartaphoenix/spapp/SpartaPhoenixApplication.java
+++ src/main/java/com/spartaphoenix/spapp/SpartaPhoenixApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpAppApplication {
+public class SpartaPhoenixApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpAppApplication.class, args);
+		SpringApplication.run(SpartaPhoenixApplication.class, args);
 	}
 
 }
